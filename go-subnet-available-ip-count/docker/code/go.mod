a9cc8cc0a6d9520d0f596909f9eb8287876c5c95
marcos-roberto <marcosroberto1808@gmail.com>
2022-01-07 11:47:19 -0300

    update project

--- go-subnet-available-ip-count/docker/code/go.mod
+++ go-subnet-available-ip-count/docker/code/go.mod
@@ -1,3 +1,8 @@
 module go-subnet-available-ip-count
 
 go 1.13
+
+require (
+	github.com/aws/aws-sdk-go v1.42.27
+	github.com/prometheus/client_golang v1.11.0
+)
--- go-subnet-available-ip-count/docker/code/main.go
+++ go-subnet-available-ip-count/docker/code/main.go
@@ -2,34 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/cloudwatch"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
+	// Environment Variables
 	REGION = os.Getenv("REGION")
 	VPC_ID = os.Getenv("VPC_ID")
-	NAMESPACE = os.Getenv("NAMESPACE")
 
+	// Session Object for AWS Connection
 	objSession = session.Must(session.NewSession(&aws.Config {
 		Region: aws.String(REGION),
 	}))
+
+	// Session Type variable
 	ec2Session *ec2.EC2
-	cloudwatchSession *cloudwatch.CloudWatch
 
 )
 
-func init()	{
-	ec2Session = ec2.New(objSession)
-	cloudwatchSession = cloudwatch.New(objSession)
+// Collector Type Struct
+type newSubnetCollector struct {
+	subnetAvailable *prometheus.Desc
 }
 
-// Gets a slice of subnets from the API for the specified VPC
+// Collector Constructor Method
+func newSubnetAvailableCollector() *newSubnetCollector {
+	return &newSubnetCollector{
+		subnetAvailable: prometheus.NewDesc("subnet_available_ip_count",
+			"Shows the amount of ip addresses available in the subnet.",
+			[]string{"subnet_id"}, nil,
+		),
+	}
+}
+
+// Collector Constructor Method Describe
+func (collector *newSubnetCollector) Describe(ch chan<- *prometheus.Desc) {
+
+	//Update this section with the each metric you create for a given collector
+	ch <- collector.subnetAvailable
+}
+
+// Collector Constructor Method Collect
+func (collector *newSubnetCollector) Collect(ch chan<- prometheus.Metric) {
+
+	// Call AWS API and save in resultMAP
+	resultMap, err := getSubnetAvailableIpAddressCountInVPC(VPC_ID, ec2Session)
+	fmt.Println(resultMap, err)
+	fmt.Println("Last Execution Time:", time.Now().Format("2022-01-02 15:04:05"))
+
+	// Write values from resultMap to the metric channel
+	for subnet, value := range resultMap {
+		//Write latest value for each metric in the prometheus metric channel.
+		//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
+		//ch <- prometheus.MustNewConstMetric(collector.subnetAvailable, prometheus.CounterValue, metricValue)
+		ch <- prometheus.MustNewConstMetric(collector.subnetAvailable, prometheus.GaugeValue, float64(value), subnet)
+	}
+}
+
+// Init variables
+func init()	{
+	ec2Session = ec2.New(objSession)
+
+}
+
+// Gets a slice of subnets from the AWS API for the specified VPC
 func getSubnetsInVPC(vpcid string, ec2client *ec2.EC2) ([]*ec2.Subnet, error) {
 	subnetReq := ec2.DescribeSubnetsInput{
 		Filters: []*ec2.Filter{
@@ -54,57 +99,22 @@ func getSubnetAvailableIpAddressCountInVPC(vpcid string, ec2client *ec2.EC2) (ma
 	if err != nil {
 		return map[string]int64{}, err
 	}
-
 	tmpMap := make(map[string]int64)
-
 	for _, subnet := range subnets {
 		tmpMap[*subnet.SubnetId] = *subnet.AvailableIpAddressCount
 	}
-
 	return tmpMap, nil
 }
 
-// Generate the metric for each subnet in cloudwatch
-func putCloudWatchMetrics(inputMap map[string]int64) {
-
-	for key, element := range inputMap {
-		_, err := cloudwatchSession.PutMetricData(&cloudwatch.PutMetricDataInput{
-			Namespace: aws.String(NAMESPACE),
-			MetricData: []*cloudwatch.MetricDatum{
-				&cloudwatch.MetricDatum{
-					MetricName: aws.String("AvailableIpAddressCount"),
-					Unit:       aws.String("Count"),
-					Value:      aws.Float64(float64(element)),
-					Dimensions: []*cloudwatch.Dimension{
-						&cloudwatch.Dimension{
-							Name:  aws.String("Subnet ID"),
-							Value: aws.String(key),
-						},
-					},
-				},
-			},
-		})
-		if err != nil {
-			fmt.Println("Error adding metrics:", err.Error())
-			return
-		}
-	}
-}
-
-func Handler() {
-	resultMap, err := getSubnetAvailableIpAddressCountInVPC(VPC_ID, ec2Session)
-	fmt.Println(resultMap, err)
-	putCloudWatchMetrics(resultMap)
-	fmt.Println("Last Execution Time:", time.Now().Format("2006-01-02 15:04:05"))
-}
-
 func main()	{
-	stop := false
-	for {
-		Handler()
-		time.Sleep(time.Second * 300)
-		if stop {
-			break
-		}
-	}
+
+	// Start the Custom Collector
+	getNewData := newSubnetAvailableCollector()
+	// Register the Collector
+	prometheus.MustRegister(getNewData)
+	// The Handler function provides a default handler to expose metrics
+	// via an HTTP server. "/metrics" is the usual endpoint for that.
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":9000", nil))
+
 }
